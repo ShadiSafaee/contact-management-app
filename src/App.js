3eca8a8a49e4cf58b4d0113f5859de5fe7710fcb
Shadi <89422699+ShadiSafaee@users.noreply.github.com>
2022-09-12 23:23:53 +0100

    bootstrap prep

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <h3>Contact Management Web App</h3>
+      <button className="btn btn-primary"></button>
     </div>
   );
 };
