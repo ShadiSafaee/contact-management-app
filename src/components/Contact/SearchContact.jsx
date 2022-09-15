2bf253942c4432021272d533ba894ae149d8a47d
Shadi <89422699+ShadiSafaee@users.noreply.github.com>
2022-09-15 23:34:47 +0100

    adding color pallet in js

--- src/components/Contact/SearchContact.jsx
+++ src/components/Contact/SearchContact.jsx
@@ -1,16 +1,18 @@
+import { Purple, Comment } from "../../helpers/colors.js";
+
 const SearchContact = () => {
   return (
     <div className="input-group mx-2 w-75">
       <span
         className="input-group-text"
         id="basic-addon1"
-        style={{ backgroundColor: "green" }}
+        style={{ backgroundColor: Purple }}
       >
         <i className="fas fa-search" />
       </span>
       <input
         type="text"
-        style={{ backgroundColor: "yellow", borderColor: "blue" }}
+        style={{ backgroundColor: Comment, borderColor: Purple }}
         className="form-control"
         placeholder="search contact..."
         aria-label="Search"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import SearchContact from "./Contact/SearchContact";
-
+import { Purple, Background } from "../helpers/colors.js";
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-dark navbar-expand-sm shadow-lg">
+    <nav
+      className="navbar navbar-dark navbar-expand-sm shadow-lg"
+      style={{ backgroundColor: Background }}
+    >
       <div className="container">
         <div className="row w-100">
           <div className="col">
-            <i className="fas fa-id-badge" />
-            <span style={{ color: "green" }}>Contact</span> Management
-            Application
+            <div className="navbar-brand">
+              <i className="fas fa-id-badge" Style={{ color: Purple }} />{" "}
+              <span style={{ color: Purple }}>Contact</span> Management
+              Application
+            </div>
           </div>
           <div className="col">
             <SearchContact />
